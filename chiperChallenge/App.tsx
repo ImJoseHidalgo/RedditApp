57709530ee56ebb37c83c87495e3b96e45002220
imjosehidalgo <josehidalgo990@gmail.com>
2021-07-22 23:57:51 -0400

    statusbar color added

--- chiperChallenge/App.tsx
+++ chiperChallenge/App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StatusBar, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 
 import { NavigationController } from './src/navigation/NavigationController';
@@ -9,6 +9,7 @@ const App = () => {
 
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor='black' />
       <View style={ styles.header }>
         <Image source={{ uri: 'https://www.reddiquette.com/wp-content/uploads/2020/09/What-Is-The-Reddit-Logo-Called.png' }} style={ styles.logo } />
       </View>
--- chiperChallenge/readme.md
+++ chiperChallenge/readme.md
@@ -1,8 +1,8 @@
 # Basic Reddit App with React Native CLI
 
-<p align="center">
-  <img src="https://res.cloudinary.com/dwqr4s7gp/image/upload/v1627011449/redit_f4x357.jpg" height="400">
-</p>
+<div align="center">
+  <img src="https://res.cloudinary.com/dwqr4s7gp/image/upload/v1627012519/reddit_ready_oghm9k.jpg" >
+</div>
 
 
 This application was built with:
